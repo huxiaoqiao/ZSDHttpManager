b9bc55f804bbaa5185934a64c6b1e0e67df3f809
huxiaoqiao <huxiaoqiao@huxiaoqiaodeMac-mini.local>
2014-10-20 14:45:57 +0800

    Initial Commit

---
+++ ZSDHttpManager/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZSDHttpManager
+//
+//  Created by 胡晓桥 on 14/10/20.
+//  Copyright (c) 2014年 Qian100. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZSDHttpManager/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZSDHttpManager
+//
+//  Created by 胡晓桥 on 14/10/20.
+//  Copyright (c) 2014年 Qian100. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
